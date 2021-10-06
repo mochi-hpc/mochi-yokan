6983c206ccb2cd5ff7968f7d218559b49fcc1dc3
Matthieu Dorier <mdorier@anl.gov>
2021-10-06 12:17:44 +0000

    added tests for Python Admin interface

--- python/py-rkv-common.cpp
+++ python/py-rkv-common.cpp
@@ -1,6 +1,7 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 #include <rkv/cxx/rkv-exception.hpp>
+#include <iostream>
 
 namespace py = pybind11;
 using namespace pybind11::literals;
@@ -16,6 +17,15 @@ PYBIND11_MODULE(pyrkv_common, m) {
             char id_str[37];
             rkv_database_id_to_string(id, id_str);
             return std::string(id_str, 36);
+        })
+        .def("__eq__", [](const rkv_database_id_t& id1, const rkv_database_id_t& id2) {
+                std::cerr << "Checking equality" << std::endl;
+            return uuid_compare(id1.uuid, id2.uuid) == 0;
+        })
+        .def("__hash__", [](const rkv_database_id_t& id) {
+                auto data = reinterpret_cast<const uint64_t*>(id.uuid);
+                uint64_t h = (~data[0]) ^ data[1];
+            return h;
         })
         .def_static("from_str", [](const std::string& str) {
             if(str.size() != 36) {
--- tests/python/test-admin.py
+++ tests/python/test-admin.py
@@ -29,14 +29,90 @@ class TestAdmin(unittest.TestCase):
         self.engine.finalize()
 
     def test_init_admin(self):
+        """Test that we can create an Admin object."""
         admin = Admin(mid=self.mid)
 
     def test_open_database(self):
+        """Test that we can open a database on the provider."""
         admin = Admin(mid=self.mid)
         admin.open_database(
             address=self.hg_addr,
             provider_id=self.provider_id,
             token='', type='map', config='{}')
+
+    def test_list_databases(self):
+        """Test that we can list databases."""
+        admin = Admin(mid=self.mid)
+        db_ids = []
+        for i in range(0,10):
+            db_id = admin.open_database(
+                address=self.hg_addr,
+                provider_id=self.provider_id,
+                token='', type='map', config='{}')
+            db_ids.append(db_id)
+        listed_ids = admin.list_databases(
+            address=self.hg_addr,
+            provider_id=self.provider_id,
+            token='')
+        self.assertEqual(set(db_ids), set(listed_ids))
+
+    def test_close_database(self):
+        """Test that we can close a database."""
+        admin = Admin(mid=self.mid)
+        db_id1 = admin.open_database(
+            address=self.hg_addr,
+            provider_id=self.provider_id,
+            token='', type='map', config='{}')
+        db_id2 = admin.open_database(
+            address=self.hg_addr,
+            provider_id=self.provider_id,
+            token='', type='map', config='{}')
+        listed_ids = admin.list_databases(
+            address=self.hg_addr,
+            provider_id=self.provider_id,
+            token='')
+        self.assertIn(db_id1, listed_ids)
+        self.assertIn(db_id2, listed_ids)
+        admin.close_database(
+            address=self.hg_addr,
+            provider_id=self.provider_id,
+            database_id=db_id1,
+            token='')
+        listed_ids = admin.list_databases(
+            address=self.hg_addr,
+            provider_id=self.provider_id,
+            token='')
+        self.assertNotIn(db_id1, listed_ids)
+        self.assertIn(db_id2, listed_ids)
+
+    def test_destroy_database(self):
+        """Test that we can destroy a database."""
+        admin = Admin(mid=self.mid)
+        db_id1 = admin.open_database(
+            address=self.hg_addr,
+            provider_id=self.provider_id,
+            token='', type='map', config='{}')
+        db_id2 = admin.open_database(
+            address=self.hg_addr,
+            provider_id=self.provider_id,
+            token='', type='map', config='{}')
+        listed_ids = admin.list_databases(
+            address=self.hg_addr,
+            provider_id=self.provider_id,
+            token='')
+        self.assertIn(db_id1, listed_ids)
+        self.assertIn(db_id2, listed_ids)
+        admin.destroy_database(
+            address=self.hg_addr,
+            provider_id=self.provider_id,
+            database_id=db_id1,
+            token='')
+        listed_ids = admin.list_databases(
+            address=self.hg_addr,
+            provider_id=self.provider_id,
+            token='')
+        self.assertNotIn(db_id1, listed_ids)
+        self.assertIn(db_id2, listed_ids)
 
 
 if __name__ == '__main__':
