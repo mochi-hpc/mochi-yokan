c036614aeb40ecd83c83e49a9cfd911d9dc031b9
Matthieu Dorier <mdorier@anl.gov>
2021-10-10 15:42:13 +0000

    finished testing put and get variants in python

--- tests/python/test-put-get.py
+++ tests/python/test-put-get.py
@@ -2,11 +2,14 @@ import os
 import sys
 import unittest
 import json
+import string
+import random
 
 wd = os.getcwd()
 sys.path.append(wd+'/../python')
 
 from pymargo.core import Engine
+import pyrkv_common as rkv
 from pyrkv_admin import Admin
 from pyrkv_client import Client
 from pyrkv_server import Provider
@@ -31,6 +34,14 @@ class TestPutGet(unittest.TestCase):
             address=self.hg_addr,
             provider_id=self.provider_id,
             database_id=db_id)
+        self.reference = dict()
+        letters = string.ascii_letters
+        for i in range(0,8):
+            key_len = random.randint(8, 64)
+            val_len = random.randint(16, 128)
+            key = ''.join(random.choice(letters) for i in range(key_len))
+            val = ''.join(random.choice(letters) for i in range(val_len))
+            self.reference[key] = val
 
     def tearDown(self):
         del self.db
@@ -39,28 +50,34 @@ class TestPutGet(unittest.TestCase):
         del self.client
         del self.mid
         del self.provider
+        del self.reference
         self.engine.finalize()
 
     def test_put_get_strings(self):
-        """Test that we can put string key/value pairs."""
-        key = 'matthieu'
-        in_val = 'dorier'
-        self.db.put(key=key, value=in_val)
+        """Test that we can put and get string key/value pairs."""
+        for k, v in self.reference.items():
+            self.db.put(key=k, value=v)
+        out_val = bytearray(128)
+        for k, v in self.reference.items():
+            vsize = self.db.get(key=k, value=out_val)
+            self.assertEqual(out_val[0:vsize].decode("ascii"), v)
 
-        out_val = bytearray(len(in_val))
-        self.db.get(key=key, value=out_val)
-        self.assertEquals(out_val.decode("utf-8"), in_val)
+        with self.assertRaises(rkv.Exception):
+            self.db.get(key='xxxxx', value=out_val)
 
-    def test_put_get_bytes(self):
-        """Test that we can put string key/value pairs."""
-        key = b'matthieu'
-        in_val = b'dorier'
-        self.db.put(key=key, value=in_val)
+    def test_put_get_buffers(self):
+        """Test that we can put and get buffer key/value pairs."""
+        for k, v in self.reference.items():
+            self.db.put(key=bytearray(k.encode('ascii')),
+                        value=bytearray(v.encode('ascii')))
+        out_val = bytearray(128)
+        for k, v in self.reference.items():
+            vsize = self.db.get(key=bytearray(k.encode('ascii')),
+                                value=out_val)
+            self.assertEqual(out_val[0:vsize].decode("ascii"), v)
 
-        out_val = bytearray(len(in_val))
-        self.db.get(key=key, value=out_val)
-        self.assertEquals(out_val.decode("utf-8"),
-                          in_val.decode("utf-8"))
+        with self.assertRaises(rkv.Exception):
+            self.db.get(key=bytearray(b'xxxxx'), value=out_val)
 
     def test_put_get_partial(self):
         """Test that we can put/get partial regions of bytearrays."""
@@ -71,8 +88,84 @@ class TestPutGet(unittest.TestCase):
         out_key = b'YYmatthieuYY'
         out_val = bytearray(len('dorier')+2)
         self.db.get(key=out_key[2:10], value=memoryview(out_val)[1:7])
-        self.assertEquals(out_val[1:7].decode("utf-8"),
-                in_val[5:11].decode("utf-8"))
+        self.assertEqual(out_val[1:7].decode('ascii'),
+                in_val[5:11].decode('ascii'))
+
+    def test_put_get_multi_strings(self):
+        """Test that we can use put_multi and get_multi with strings."""
+        pairs = list(self.reference.items())
+        self.db.put_multi(pairs=pairs)
+
+        out = []
+        for k in self.reference:
+            out.append((k, bytearray(128)))
+        out.append(('xxxxxxxx', bytearray(128)))
+        random.shuffle(out)
+
+        vsizes = self.db.get_multi(pairs=out)
+        for i, (k, v) in enumerate(out):
+            if k == 'xxxxxxxx':
+                self.assertIsNone(vsizes[i])
+            else:
+                self.assertEqual(v[0:vsizes[i]].decode('ascii'),
+                                  self.reference[k])
+
+    def test_put_get_multi_buffers(self):
+        """Test that we can use put_multi and get_multi with buffers."""
+        pairs = [ (bytearray(k.encode('ascii')), bytearray(v.encode('ascii'))) for k, v in self.reference.items() ]
+        self.db.put_multi(pairs=pairs)
+
+        out = []
+        for k in self.reference:
+            out.append((bytearray(k.encode('ascii')), bytearray(128)))
+        out.append((bytearray(b'xxxxxxxx'), bytearray(128)))
+        random.shuffle(out)
+
+        vsizes = self.db.get_multi(pairs=out)
+        for i, (k, v) in enumerate(out):
+            if k.decode('ascii') == 'xxxxxxxx':
+                self.assertIsNone(vsizes[i])
+            else:
+                self.assertEqual(v[0:vsizes[i]].decode('ascii'),
+                                  self.reference[k.decode('ascii')])
+
+    def test_put_get_packed(self):
+        """Test that we can use put_packed and get_packed."""
+        in_keys_buf = bytearray()
+        in_vals_buf = bytearray()
+        in_key_sizes = []
+        in_val_sizes = []
+        for k, v in self.reference.items():
+            in_keys_buf += bytearray(k.encode('ascii'))
+            in_key_sizes.append(len(k))
+            in_vals_buf += bytearray(v.encode('ascii'))
+            in_val_sizes.append(len(v))
+
+        self.db.put_packed(keys=in_keys_buf, key_sizes=in_key_sizes,
+                           values=in_vals_buf, value_sizes=in_val_sizes)
+
+        out_keys = list(self.reference.keys())
+        out_keys.append('xxxxxxxx')
+        random.shuffle(out_keys)
+        out_keys_buf = bytearray()
+        out_key_sizes = []
+        for k in out_keys:
+            out_keys_buf += k.encode('ascii')
+            out_key_sizes.append(len(k))
+        out_vals_buf = bytearray(128*len(out_key_sizes))
+
+        out_val_sizes = self.db.get_packed(keys=out_keys_buf, key_sizes=out_key_sizes,
+                                           values=out_vals_buf)
+        voffset = 0
+        for i, k in enumerate(out_keys):
+            if k == 'xxxxxxxx':
+                self.assertIsNone(out_val_sizes[i])
+                continue
+            vsize = out_val_sizes[i]
+            self.assertEqual(out_vals_buf[voffset:voffset+vsize].decode('ascii'),
+                             self.reference[k])
+            voffset += vsize
+
 
 if __name__ == '__main__':
     unittest.main()
