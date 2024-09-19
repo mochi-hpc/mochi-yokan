1c3b8e1386194df1703a7db86abd6b0ddb01e55d
Matthieu Dorier <mdorier@anl.gov>
2024-09-19 14:49:03 +0100

    updated bedrock module to latest API (remains to be tested)

--- src/bedrock/bedrock-module.cpp
+++ src/bedrock/bedrock-module.cpp
@@ -1,149 +1,96 @@
 /*
- * (C) 2021 The University of Chicago
+ * (C) 2024 The University of Chicago
  *
  * See COPYRIGHT in top-level directory.
  */
-#include <bedrock/module.h>
-#include <nlohmann/json.hpp>
-#include <string.h>
-#include <stdlib.h>
-#include "yokan/server.h"
-#include "yokan/client.h"
-#include "yokan/database.h"
-#include "../client/client.hpp"
 
-static int yk_bedrock_register_provider(
-        bedrock_args_t args,
-        bedrock_module_provider_t* provider)
-{
-    margo_instance_id mid = bedrock_args_get_margo_instance(args);
-    uint16_t provider_id  = bedrock_args_get_provider_id(args);
+#include "yokan/cxx/server.hpp"
+#include <bedrock/AbstractComponent.hpp>
 
-    struct yk_provider_args yk_args = YOKAN_PROVIDER_ARGS_INIT;
-    const char* config = bedrock_args_get_config(args);
-    yk_args.pool       = bedrock_args_get_pool(args);
+namespace tl = thallium;
 
-    // these are optional, but if they are not present,
-    // bedrock_args_get_dependency will return NULL.
-    yk_args.remi.provider = (remi_provider_t)bedrock_args_get_dependency(args, "remi_provider", 0);
-    yk_args.remi.client   = (remi_client_t)bedrock_args_get_dependency(args, "remi_client", 0);
+class YokanComponent : public bedrock::AbstractComponent {
 
-    return yk_provider_register(mid, provider_id, config, &yk_args,
-                                (yk_provider_t*)provider);
-}
+    std::unique_ptr<yokan::Provider> m_provider;
 
-static int yk_bedrock_deregister_provider(
-        bedrock_module_provider_t provider)
-{
-    return yk_provider_destroy((yk_provider_t)provider);
-}
+    public:
 
-static char* yk_bedrock_get_provider_config(
-        bedrock_module_provider_t provider) {
-    return yk_provider_get_config((yk_provider_t)provider);
-}
+    YokanComponent(const tl::engine& engine,
+                   uint16_t  provider_id,
+                   const std::string& config,
+                   const yk_provider_args* args)
+    : m_provider{
+        std::make_unique<yokan::Provider>(
+            engine.get_margo_instance(), provider_id, config.c_str(), args)}
+    {}
 
-static int yk_bedrock_init_client(
-        bedrock_args_t args,
-        bedrock_module_client_t* client)
-{
-    margo_instance_id mid = bedrock_args_get_margo_instance(args);
-    return yk_client_init(mid, (yk_client_t*)client);
-}
-
-static int yk_bedrock_finalize_client(
-        bedrock_module_client_t client)
-{
-    return yk_client_finalize((yk_client_t)client);
-}
-
-static char* yk_bedrock_get_client_config(
-        bedrock_module_client_t client) {
-    (void)client;
-    // TODO
-    return strdup("{}");
-}
-
-static int yk_bedrock_create_provider_handle(
-        bedrock_module_client_t client,
-        hg_addr_t address,
-        uint16_t provider_id,
-        bedrock_module_provider_handle_t* ph)
-{
-    yk_client_t c = (yk_client_t)client;
-    yk_database_handle_t db;
-    yk_return_t ret = yk_database_handle_create(c, address, provider_id, true, &db);
-    if(ret != YOKAN_SUCCESS) return ret;
-    *ph = (bedrock_module_provider_handle_t)db;
-    return BEDROCK_SUCCESS;
-}
-
-static int yk_bedrock_destroy_provider_handle(
-        bedrock_module_provider_handle_t ph)
-{
-    yk_database_handle_t db = (yk_database_handle_t)ph;
-    return yk_database_handle_release(db);
-}
-
-static int yk_bedrock_migrate(
-        bedrock_module_provider_t p,
-        const char* dest_addr,
-        uint16_t dest_provider_id,
-        const char* migration_config,
-        bool remove_source)
-{
-    yk_provider_t provider = (yk_provider_t)p;
-    if(!remove_source) return YOKAN_ERR_OP_UNSUPPORTED;
-
-    struct yk_migration_options options = {nullptr, nullptr, 0};
-
-    using json = nlohmann::json;
-    std::string extra_config = "{}";
-
-    json config = migration_config ? json::parse(migration_config) : json::object();
-    if(!config.is_object()) return YOKAN_ERR_INVALID_CONFIG;
-
-    if(config.contains("new_root") && config["new_root"].is_string()) {
-        options.new_root = config["new_root"].get_ref<const std::string&>().c_str();
-    }
-    if(config.contains("extra_config")) {
-        extra_config = config["extra_config"].dump();
-        options.extra_config = extra_config.c_str();
-    }
-    if(config.contains("xfer_size") && config["xfer_size"].is_number_unsigned()) {
-        options.xfer_size = config["xfer_size"].get<size_t>();
+    void* getHandle() override {
+        return static_cast<void*>(m_provider.get());
     }
 
-    yk_return_t ret = yk_provider_migrate_database(
-        provider, dest_addr, dest_provider_id, &options);
+    std::string getConfig() override {
+        return m_provider->getConfig();
+    }
 
-    return ret;
-}
+    static std::shared_ptr<bedrock::AbstractComponent>
+        Register(const bedrock::ComponentArgs& args) {
+            tl::pool pool;
+            auto it = args.dependencies.find("pool");
+            if(it != args.dependencies.end() && !it->second.empty()) {
+                pool = it->second[0]->getHandle<tl::pool>();
+            }
+            remi_client_t remi_sender = nullptr;
+            it = args.dependencies.find("remi_sender");
+            if(it != args.dependencies.end() && !it->second.empty()) {
+                auto component = it->second[0]->getHandle<bedrock::ComponentPtr>();
+                remi_sender = static_cast<remi_client_t>(component->getHandle());
+            }
+            remi_provider_t remi_receiver = nullptr;
+            it = args.dependencies.find("remi_receiver");
+            if(it != args.dependencies.end() && !it->second.empty()) {
+                auto component = it->second[0]->getHandle<bedrock::ComponentPtr>();
+                remi_receiver = static_cast<remi_provider_t>(component->getHandle());
+            }
+            yk_provider_args yk_args = {
+                /* .pool = */ pool.native_handle(),
+                /* .cache = */ nullptr,
+                /* .remi = */ {
+                    /* .client = */ remi_sender,
+                    /* .provider = */ remi_receiver
+                }
+            };
+            return std::make_shared<YokanComponent>(
+                args.engine, args.provider_id, args.config, &yk_args);
+        }
 
-static struct bedrock_dependency yokan_provider_dependencies[] = {
-    { "remi_provider", "remi", BEDROCK_OPTIONAL },
-    { "remi_client", "remi", BEDROCK_OPTIONAL },
-    BEDROCK_NO_MORE_DEPENDENCIES
+    static std::vector<bedrock::Dependency>
+        GetDependencies(const bedrock::ComponentArgs& args) {
+            (void)args;
+            std::vector<bedrock::Dependency> dependencies{
+                bedrock::Dependency{
+                    /* name */ "pool",
+                    /* type */ "pool",
+                    /* is_required */ false,
+                    /* is_array */ false,
+                    /* is_updatable */ false
+                },
+                bedrock::Dependency{
+                    /* name */ "remi_sender",
+                    /* type */ "remi_sender",
+                    /* is_required */ false,
+                    /* is_array */ false,
+                    /* is_updatable */ false
+                },
+                bedrock::Dependency{
+                    /* name */ "remi_receiver",
+                    /* type */ "remi_receiver",
+                    /* is_required */ false,
+                    /* is_array */ false,
+                    /* is_updatable */ false
+                }
+            };
+            return dependencies;
+        }
 };
 
-static struct bedrock_module_v3 yokan = {
-    3,
-    yk_bedrock_register_provider,
-    yk_bedrock_deregister_provider,
-    yk_bedrock_get_provider_config,
-    yk_bedrock_init_client,
-    yk_bedrock_finalize_client,
-    yk_bedrock_get_client_config,
-    yk_bedrock_create_provider_handle,
-    yk_bedrock_destroy_provider_handle,
-    yokan_provider_dependencies,
-    /* client dependencies */ NULL,
-    /* change_provider_pool */ NULL,
-    /* snapshot_provider */ NULL,
-    /* restore_provider */ NULL,
-    yk_bedrock_migrate,
-    /* get_provider_dependencies */ NULL,
-    /* get_client_dependencies */ NULL
-};
-
-extern "C" BEDROCK_REGISTER_MODULE_WITH_VERSION(yokan, yokan, 3)
+BEDROCK_REGISTER_COMPONENT_TYPE(yokan, YokanComponent)
