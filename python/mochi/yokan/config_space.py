e90f9b96d4e15f8db20131b191c9ee0f83b0edf8
Matthieu Dorier <mdorier@anl.gov>
2024-09-20 15:39:11 +0100

    updated config space

---
+++ python/mochi/yokan/config_space.py
@@ -0,0 +1,109 @@
+# (C) 2024 The University of Chicago
+# See COPYRIGHT in top-level directory.
+
+
+"""
+.. module:: config_space
+   :synopsis: This package provides the configuration for a Yokan provider
+   and the corresponding ConfigurationSpace.
+
+.. moduleauthor:: Matthieu Dorier <mdorier@anl.gov>
+
+
+"""
+
+
+from dataclasses import dataclass
+from typing import Optional
+from dataclasses import dataclass
+from mochi.bedrock.spec import (
+        ProviderConfigSpaceBuilder,
+        CS,
+        Config,
+        ProviderSpec)
+from mochi.bedrock.config_space import (
+        CategoricalChoice,
+        ConfigurationSpace,
+        InCondition,
+        Categorical,
+        CategoricalOrConst)
+
+
+@dataclass(frozen=True)
+class BackendType:
+
+    name: str
+    is_sorted: bool = True
+    has_values: bool = True
+    is_persistent: bool = True
+
+
+class YokanSpaceBuilder(ProviderConfigSpaceBuilder):
+
+    _backends = [
+        BackendType(name='map', is_persistent=False),
+        BackendType(name='unordered_map', is_sorted=False, is_persistent=False),
+        BackendType(name='set', is_persistent=False, has_values=False),
+        BackendType(name='unordered_set', is_sorted=False, is_persistent=False, has_values=False),
+        BackendType(name='rocksdb'),
+        BackendType(name='leveldb'),
+        BackendType(name='berkeleydb'),
+        BackendType(name='lmdb'),
+        BackendType(name='tkrzw'),
+        BackendType(name='unqlite'),
+        BackendType(name='gdbm', is_sorted=False),
+    ]
+
+    def __init__(self, *,
+                 types: list[str] = ['map', 'unordered_map', 'set', 'unordered_set', 'rocksdb',
+                                    'leveldb', 'berkeleydb', 'lmdb', 'tkrzw', 'unqlite', 'gdbm'],
+                 paths: list[str] = [],
+                 need_sorted_db: bool = True,
+                 need_values: bool = True,
+                 need_persistence: bool = False,
+                 tags: list[str] = []):
+        from .backends import available_backends
+        if need_persistence and len(paths) == 0:
+            raise ValueError("Paths should be provided if database needs persistence")
+        self.types = [b for b in YokanSpaceBuilder._backends if \
+            (b.name in available_backends) and \
+            ((not need_sorted_db) or b.is_sorted) and \
+            ((not need_values) or b.has_values) and \
+            ((not need_persistence) or b.is_persistent) and \
+            b.name in types]
+        self.paths = paths
+        self.tags = tags
+
+    def set_provider_hyperparameters(self, configuration_space: CS) -> None:
+        from mochi.bedrock.config_space import (
+                ConfigurationSpace, InCondition, Categorical, CategoricalOrConst)
+        # add a pool dependency
+        num_pools = configuration_space["margo.argobots.num_pools"]
+        configuration_space.add(CategoricalChoice("pool", num_options=num_pools))
+        # add backend type
+        hp_type = Categorical('type', self.types)
+        configuration_space.add(hp_type)
+        # add path
+        if len(self.paths) != 0:
+            hp_path = CategoricalOrConst("path", self.paths)
+            configuration_space.add(hp_path)
+            path_if_persistent = InCondition(hp_path, hp_type, [t for t in self.types if t.is_persistent])
+            configuration_space.add(path_if_persistent)
+
+    def resolve_to_provider_spec(self, name: str, provider_id: int,
+                                 config: Config, prefix: str) -> ProviderSpec:
+        type_key = prefix + "type"
+        path_key = prefix + "path"
+        db_type = config[prefix + "type"]
+        cfg = {
+            "type" : db_type.name
+        }
+        if path_key in config:
+            cfg["config"] = {
+                "path": config[path_key]
+            }
+        dep = {
+            "pool" : int(config[prefix + "pool"])
+        }
+        return ProviderSpec(name=name, type="yokan", provider_id=provider_id,
+                            tags=["tag1", "tag2"], config=cfg, dependencies=dep)
--- python/mochi/yokan/test_config_space.py
+++ python/mochi/yokan/test_config_space.py
@@ -3,36 +3,46 @@
 
 
 import unittest
-from .spec import YokanProviderSpec
+from .config_space import YokanSpaceBuilder
 from mochi.bedrock.spec import ProcSpec
+
 
 class TestConfigSpace(unittest.TestCase):
 
-    def test_yokan_config_space(self):
-
-        max_num_pools = 3
-
-        ycs = YokanProviderSpec.space(
-            max_num_pools=max_num_pools,
-            paths=["/tmp", "/scratch"],
-            need_values=False, need_persistence=False,
-            tags=['my_tag'])
-
+    def test_yokan_provider_space(self):
+        yokan_space_builder = YokanSpaceBuilder()
         provider_space_factories = [
             {
                 "family": "databases",
-                "space": ycs,
+                "builder": yokan_space_builder,
                 "count": (1,3)
             }
         ]
-
-        space = ProcSpec.space(num_pools=(1, max_num_pools), num_xstreams=(2, 5),
+        space = ProcSpec.space(num_pools=(1,3), num_xstreams=(2,5),
                                provider_space_factories=provider_space_factories).freeze()
-        print(space)
+        #print(space)
         config = space.sample_configuration()
-        print(config)
+        #print(config)
         spec = ProcSpec.from_config(address='na+sm', config=config)
-        print(spec.to_json(indent=4))
+        #print(spec.to_json(indent=4))
+
+    def test_yokan_provider_space_with_paths(self):
+        yokan_space_builder = YokanSpaceBuilder(paths=["/aaa", "/bbb"])
+        provider_space_factories = [
+            {
+                "family": "databases",
+                "builder": yokan_space_builder,
+                "count": (1,3)
+            }
+        ]
+        space = ProcSpec.space(num_pools=(1,3), num_xstreams=(2,5),
+                               provider_space_factories=provider_space_factories).freeze()
+        #print(space)
+        config = space.sample_configuration()
+        #print(config)
+        spec = ProcSpec.from_config(address='na+sm', config=config)
+        #print(spec.to_json(indent=4))
+
 
 
 if __name__ == "__main__":
