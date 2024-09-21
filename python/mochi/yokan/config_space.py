f45f0a783aa08e7705b1bd61778a77c9ef0691bc
Matthieu Dorier <mdorier@anl.gov>
2024-09-21 11:29:59 +0100

    removed unnecessary import

--- python/mochi/yokan/config_space.py
+++ python/mochi/yokan/config_space.py
@@ -75,8 +75,6 @@ class YokanSpaceBuilder(ProviderConfigSpaceBuilder):
         self.tags = tags
 
     def set_provider_hyperparameters(self, configuration_space: CS) -> None:
-        from mochi.bedrock.config_space import (
-                ConfigurationSpace, InCondition, Categorical, CategoricalOrConst)
         # add a pool dependency
         num_pools = configuration_space["margo.argobots.num_pools"]
         configuration_space.add(CategoricalChoice("pool", num_options=num_pools))
